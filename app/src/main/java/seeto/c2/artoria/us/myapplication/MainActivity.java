3fa70251ff0aeb327fcb16a54425729ac6be94cc
Woopa01 <whdnwls49@naver.com>
2018-05-18 09:27:51 +0900

    init

---
+++ app/src/main/java/seeto/c2/artoria/us/myapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package seeto.c2.artoria.us.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //마스터에 푸시가 안됨
+    }
+}
