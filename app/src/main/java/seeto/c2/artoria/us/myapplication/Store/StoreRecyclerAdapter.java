9debf6a5e7a224af2f52f66ac625bbc581a23265
Woopa01 <whdnwls49@naver.com>
2018-07-10 15:10:20 +0900

    add store purchase dialog

--- app/src/main/java/seeto/c2/artoria/us/myapplication/Store/StoreRecyclerAdapter.java
+++ app/src/main/java/seeto/c2/artoria/us/myapplication/Store/StoreRecyclerAdapter.java
@@ -1,11 +1,15 @@
 package seeto.c2.artoria.us.myapplication.Store;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowInsets;
 import android.widget.TextView;
 
 
@@ -35,6 +39,35 @@ public class StoreRecyclerAdapter extends RecyclerView.Adapter<StoreRecyclerAdap
         holder.title.setText(items.get(position).getTitle());
         holder.content.setText(items.get(position).getContent());
         holder.price.setText(items.get(position).getPrice());
+        holder.item.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Dialog dialog = new Dialog(context);
+                View view = LayoutInflater.from(context).inflate(R.layout.dialog_store,null);
+                View cancel = view.findViewById(R.id.dialog_store_cancel);
+                View purchase = view.findViewById(R.id.dialog_store_purchase);
+                dialog.setContentView(view);
+                dialog.show();
+
+                cancel.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+
+                purchase.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                    }
+                });
+
+                dialog.setCanceledOnTouchOutside(false);
+                Window window = dialog.getWindow();
+                window.setLayout(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+            }
+        });
     }
 
     @Override
@@ -46,12 +79,14 @@ public class StoreRecyclerAdapter extends RecyclerView.Adapter<StoreRecyclerAdap
         private TextView title;
         private TextView content;
         private TextView price;
+        private ConstraintLayout item;
 
         public CustomView(View itemView) {
             super(itemView);
             title = itemView.findViewById(R.id.item_store_title);
             content = itemView.findViewById(R.id.item_store_content);
             price = itemView.findViewById(R.id.item_store_price);
+            item = itemView.findViewById(R.id.item_store_item);
         }
     }
 }
