6f746d32d5f9497595ba7e852c9cc0a03b1401a3
chaminji <minjicha0319@naver.com>
2018-08-05 11:30:55 +0900

    ahahahhhh

--- app/src/main/java/seeto/c2/artoria/us/myapplication/QM/ViewMemoActivity.java
+++ app/src/main/java/seeto/c2/artoria/us/myapplication/QM/ViewMemoActivity.java
@@ -5,7 +5,11 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 import seeto.c2.artoria.us.myapplication.R;
 
@@ -15,6 +19,7 @@ public class ViewMemoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_viewmemo);
+        TextView viewmemo = (TextView)findViewById(R.id.viewMemo);
         final FloatingActionButton editfab = (FloatingActionButton) findViewById(R.id.editFab);
         final FloatingActionButton checkfab = (FloatingActionButton) findViewById(R.id.checkFab);
 
@@ -34,5 +39,4 @@ public class ViewMemoActivity extends AppCompatActivity {
             }
         });
     }
-
 }
