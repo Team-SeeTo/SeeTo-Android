9872bd87ddad326b1f2292b6a2a2e12bf720fe96
Woopa01 <whdnwls49@naver.com>
2018-08-13 23:09:30 +0900

    fixed BasePresenter

--- app/src/main/java/seeto/c2/artoria/us/myapplication/BasePresenter.java
+++ app/src/main/java/seeto/c2/artoria/us/myapplication/BasePresenter.java
@@ -3,7 +3,7 @@ package seeto.c2.artoria.us.myapplication;
 public class BasePresenter implements BaseView {
 
     @Override
-    public void showToast() {
+    public void showToast(String text) {
 
     }
 }
