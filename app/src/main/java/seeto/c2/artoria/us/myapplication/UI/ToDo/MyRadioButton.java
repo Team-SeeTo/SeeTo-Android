d2c13e6c7a80a1c32f1e026da5e13a3e575f0be1
khg0712 <k7120792@gmail.com>
2018-09-13 08:26:16 +0900

    radio button 완료 시 소리 설정

--- app/src/main/java/seeto/c2/artoria/us/myapplication/UI/ToDo/MyRadioButton.java
+++ app/src/main/java/seeto/c2/artoria/us/myapplication/UI/ToDo/MyRadioButton.java
@@ -1,20 +1,29 @@
 package seeto.c2.artoria.us.myapplication.UI.ToDo;
 
 import android.content.Context;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.util.AttributeSet;
+import android.util.Log;
 
 public class MyRadioButton extends android.support.v7.widget.AppCompatRadioButton{
 
+    private Context context;
+
     public MyRadioButton(Context context) {
         super(context);
+         this.context = context;
     }
 
     public MyRadioButton(Context context, AttributeSet attrs) {
         super(context, attrs);
+        this.context = context;
     }
 
     public MyRadioButton(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        this.context = context;
     }
 
     @Override
@@ -23,6 +32,11 @@ public class MyRadioButton extends android.support.v7.widget.AppCompatRadioButto
             setChecked(false);
         } else {
             setChecked(true);
+            //TODO: 알람음 설정
+            Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+            Ringtone r = RingtoneManager.getRingtone(this.context, notification);
+            r.play();
+            Log.d("toggle","error");
         }
     }
 }
