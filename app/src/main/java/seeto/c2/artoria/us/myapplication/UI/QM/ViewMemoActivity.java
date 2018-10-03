3622313ee9a1ab355391f2055147c3c6b6e5b9dc
chaminji <minjicha0319@naver.com>
2018-10-03 19:17:17 +0900

    final

--- app/src/main/java/seeto/c2/artoria/us/myapplication/UI/QM/ViewMemoActivity.java
+++ app/src/main/java/seeto/c2/artoria/us/myapplication/UI/QM/ViewMemoActivity.java
@@ -1,9 +1,14 @@
 package seeto.c2.artoria.us.myapplication.UI.QM;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
@@ -17,11 +22,15 @@ import com.orhanobut.dialogplus.DialogPlus;
 import com.orhanobut.dialogplus.DialogPlusBuilder;
 import com.orhanobut.dialogplus.ViewHolder;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import seeto.c2.artoria.us.myapplication.R;
 import seeto.c2.artoria.us.myapplication.UI.Main.MainActivity;
 
 public class ViewMemoActivity extends AppCompatActivity {
     int count = 0;
+    EditText viewmemo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +38,7 @@ public class ViewMemoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_viewmemo);
 
         final FloatingActionButton editfab = (FloatingActionButton) findViewById(R.id.viewMemo_editFab);
-        final EditText viewmemo = findViewById(R.id.qm_search_et);
+        viewmemo = findViewById(R.id.qm_search_et);
         ImageButton qm_search_btn = findViewById(R.id.qm_search_btn);
         ImageView memo_search_btn = findViewById(R.id.memo_search_btn);
 
@@ -38,22 +47,19 @@ public class ViewMemoActivity extends AppCompatActivity {
                 }
         );
 
-        editfab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.e("작성", "카운트: " + count);
-                if (count == 0) {
-                    editfab.setImageResource(R.drawable.memo_check);
-                    viewmemo.setFocusableInTouchMode(true);
-                    ++count;
-                } else if (count == 1) {
-                    --count;
-                    Toast.makeText(ViewMemoActivity.this, "작성을 완료합니다.", Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(ViewMemoActivity.this, MainActivity.class)
-                            .putExtra("Memo", true);
-                    startActivity(intent);
-                    viewmemo.setFocusableInTouchMode(false);
-                }
+        editfab.setOnClickListener(v -> {
+            Log.e("작성", "카운트: " + count);
+            if (count == 0) {
+                editfab.setImageResource(R.drawable.memo_check);
+                viewmemo.setFocusableInTouchMode(true);
+                ++count;
+            } else if (count == 1) {
+                --count;
+                Toast.makeText(ViewMemoActivity.this, "작성을 완료합니다.", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(ViewMemoActivity.this, MainActivity.class)
+                        .putExtra("Memo", true);
+                startActivity(intent);
+                viewmemo.setFocusableInTouchMode(false);
             }
         });
     }
@@ -69,21 +75,36 @@ public class ViewMemoActivity extends AppCompatActivity {
         ImageView memo_search_btn = (ImageView) QMdialogPlus.findViewById(R.id.memo_search_btn);
         QMdialogPlus.show();
 
-        memo_search_btn.setOnClickListener(v -> {
+        memo_search_btn.setOnClickListener((View v) -> {
+
             if (memo_search_et.getText().toString().isEmpty()) {
                 Toast.makeText(ViewMemoActivity.this, "내용을 입력하세요", Toast.LENGTH_SHORT).show();
             } else {
-                for (int i = 0; i < memo_search_et.getText().toString().length(); i++) {
-                    if (memo_search_et.getText().toString().equals("")) {
-
+                int i;
+                String memoText = viewmemo.getText().toString();
+                String searchText = memo_search_et.getText().toString();
+                SpannableStringBuilder ssb = new SpannableStringBuilder(memoText);
+                if(memoText.contains(searchText)) {
+                    for(i=0; i< memoText.length(); i++) {
+                        int findedIndex = memoText.indexOf(searchText, i);
+                        if(findedIndex < 0 || findedIndex > memoText.length() )
+                            break;
+                        Log.d("Index", "i: "+i+" finded: "+findedIndex);
+                        Log.d("text", "message: "+memoText.substring(findedIndex, findedIndex+searchText.length()));
+                        i += findedIndex;
+                        ssb.setSpan(
+                                new ForegroundColorSpan(Color.RED),
+                                findedIndex,
+                                findedIndex+searchText.length(),
+                                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+                        );
                     }
-                    else {
-                        Toast.makeText(ViewMemoActivity.this, "해당 단어를 찾을 수 없습니다", Toast.LENGTH_SHORT).show();
-                    }
+                    viewmemo.setText(ssb);
+                } else {
+                    Toast.makeText(ViewMemoActivity.this, "해당 단어를 찾을 수 없습니다", Toast.LENGTH_SHORT).show();
                 }
             }
         });
-
         memo_search_et.setFocusableInTouchMode(true);
         memo_search_et.requestFocus();
     }
