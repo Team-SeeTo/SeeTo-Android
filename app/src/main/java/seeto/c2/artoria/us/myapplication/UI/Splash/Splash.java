b14599e36391fea5baf7a5583d68d25a7682b5ca
Woopa01 <whdnwls49@naver.com>
2018-10-04 20:00:02 +0900

    add splash

---
+++ app/src/main/java/seeto/c2/artoria/us/myapplication/UI/Splash/Splash.java
@@ -0,0 +1,18 @@
+package seeto.c2.artoria.us.myapplication.UI.Splash;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import seeto.c2.artoria.us.myapplication.UI.Signin.SigninActivity;
+
+public class Splash extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(Splash.this, SigninActivity.class);
+        startActivity(intent);
+    }
+}
